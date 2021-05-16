78fabafc12fdff85a73f2204b950d8339f8de6a4
CrimeX2007 <dhruv.navneet@gmail.com>
2021-05-16 15:53:53 +0530

    Initial commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODE1MDk2MjQxMTA0NDg2NDAx.YDnbhw.KegrI1qzzzN1CvaBQgowk14doMc',
+        prefix: ',',
+        activity: 'CORONA BHAAG JA',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
